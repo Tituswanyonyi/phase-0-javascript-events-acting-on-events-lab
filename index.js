d111c93d413f4c25ccea27e3d0c6806743033879
titus <titus.unavailable@student.moringaschool.com>
2023-03-23 13:03:01 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10)
   if (left < 360) {
-    dodger.style.left = `${left + 1}px`
+    dodger.style.left = `${left + 1}px` 
   }
 }
 
@@ -23,10 +23,10 @@ document.addEventListener('keydown', function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft()
   }
-})
+}); 
 
 document.addEventListener('keydown', function(e) {
   if (e.key === "ArrowRight") {
     moveDodgerRight()
   }
-})
+}); 
